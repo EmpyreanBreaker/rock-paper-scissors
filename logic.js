a5116fadebebb69482bc65b437ac4bfecec6d04a
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2025-09-19 23:09:11 -0500

    feat: added game logic

--- logic.js
+++ logic.js
@@ -1,66 +1,81 @@
+/**Choices & Scores */
+let playerChoice = "";
+let computerChoice = "";
+
+let round = 1;
+let playerScore = 0;
+let computerScore = 0;
+
 /**Get Computer Selection*/
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
 
-    if (randomNumber == 1) return "Rock";
-    if (randomNumber == 2) return "Paper";
+    if (randomNumber == 1) return "rock";
+    if (randomNumber == 2) return "paper";
 
-    return "Scissors";
+    return "scissors";
 }
 
-/**Get Player Selection*/
+/**Get Player Selection & Play A Game*/
 const buttons = document.querySelectorAll(".game__button");
-let playerChoice = "";
 
-buttons.forEach(button => {
+buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
-        playerChoice = e.target.textContent.trim();
-        console.log("Player choice:", playerChoice);
-    });
-});
+        playerChoice = e.target.textContent.trim().toString().toLowerCase();
+        computerChoice = getComputerChoice();
+        console.log(`Player Choice: ${playerChoice}`);
+        console.log(`Computer Choice: ${computerChoice}`);
+        playRound(computerChoice, playerChoice);
+        updateScores();
+    })
+})
 
-/**Game Logic*/
-let round = 0;
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(computerChoice, humanChoice) {
-    if (humanChoice === 'rock' && computerChoice === 'paper') {
-        computerScore += 1;
-    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
-        humanScore += 1;
-    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-        humanScore += 1;
-    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-        computerScore += 1;
-    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-        computerScore += 1;
-    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-        humanScore += 1;
+/**Game Round */
+function playRound(computerChoice, playerChoice) {
+    if (playerScore !== 5 && compScore !== 5) {
+        if (playerChoice === 'rock' && computerChoice === 'paper') {
+            computerScore += 1;
+        } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+            playerScore += 1;
+        } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+            playerScore += 1;
+        } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+            computerScore += 1;
+        } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+            computerScore += 1;
+        } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+            playerScore += 1;
+        }
     }
-
     round += 1;
-    return round;
 }
+
+/** Display  & Update Scores */
+const compScore = document.querySelector(".score__computer");
+const userScore = document.querySelector(".score__player");
+
+function updateScores() {
+    compScore.textContent = `Computer Score: ${computerScore}`;
+    userScore.textContent = `Player Score: ${playerScore}`;
+}
+const results = document.querySelector(".score__output");
+
 
 function playGame() {
-    playRound(getComputerChoice(), getHumanChoice());
 
-    console.log("Human Score: " + humanScore);
-    console.log("Computer Score: " + computerScore)
+    // Play the game till someone has five points
+    playRound(getComputerChoice());
 
-    if (humanScore > computerScore) {
-        console.log("Humanity Wins!")
-    }
-    else if (computerScore < humanScore) {
-        console.log("Bow down to your AI overlords")
-    }
-    else {
-        console.log("It's a tie. Perhaps we need each other.")
-    }
+    // console.log("Player Score: " + playerScore);
+    // console.log("Computer Score: " + computerScore)
+
+    // if (playerScore > computerScore) {
+    //     console.log("Humanity Wins!")
+    // }
+    // else if (computerScore < playerScore) {
+    //     console.log("Bow down to your AI overlords")
+    // }
+    // else {
+    //     console.log("It's a tie. Perhaps we need each other.")
+    // }
 }
-
-
-
-/**Output*/
-//playGame();
