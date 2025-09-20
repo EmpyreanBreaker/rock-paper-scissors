dc89c25dfebdb6b117edaef4b49ac3f31c55928c
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2025-09-20 12:57:12 -0500

    adds: winning and game logic

--- logic.js
+++ logic.js
@@ -2,9 +2,10 @@
 let playerChoice = "";
 let computerChoice = "";
 
-let round = 1;
+let round = 0;
 let playerScore = 0;
 let computerScore = 0;
+let tie = 0;
 
 /**Get Computer Selection*/
 function getComputerChoice() {
@@ -16,23 +17,9 @@ function getComputerChoice() {
     return "scissors";
 }
 
-/**Get Player Selection & Play A Game*/
-const buttons = document.querySelectorAll(".game__button");
-
-buttons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-        playerChoice = e.target.textContent.trim().toString().toLowerCase();
-        computerChoice = getComputerChoice();
-        console.log(`Player Choice: ${playerChoice}`);
-        console.log(`Computer Choice: ${computerChoice}`);
-        playRound(computerChoice, playerChoice);
-        updateScores();
-    })
-})
-
-/**Game Round */
+/**Game Round Logic*/
 function playRound(computerChoice, playerChoice) {
-    if (playerScore !== 5 && compScore !== 5) {
+    if (playerScore < 5 && computerScore < 5) {
         if (playerChoice === 'rock' && computerChoice === 'paper') {
             computerScore += 1;
         } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
@@ -46,36 +33,43 @@ function playRound(computerChoice, playerChoice) {
         } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
             playerScore += 1;
         }
+        else {
+            tie += 1;
+        }
+        round += 1;
     }
-    round += 1;
+    else {
+        if (playerScore > computerScore) {
+            winner.textContent = "Winner: Player Wins !!!";
+        }
+        else {
+            winner.textContent = "Winner: Computer Wins!!!";
+        }
+    }
 }
+
+/**Get Player Selection & Play A Game*/
+const buttons = document.querySelectorAll(".game__button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        playerChoice = e.target.textContent.trim().toString().toLowerCase();
+        computerChoice = getComputerChoice();
+        playRound(computerChoice, playerChoice);
+        updateScores();
+    })
+})
 
 /** Display  & Update Scores */
 const compScore = document.querySelector(".score__computer");
 const userScore = document.querySelector(".score__player");
+const tiedScore = document.querySelector(".score__tied");
+const roundTotal = document.querySelector(".score__round");
+const winner = document.querySelector(".score__output");
 
 function updateScores() {
     compScore.textContent = `Computer Score: ${computerScore}`;
     userScore.textContent = `Player Score: ${playerScore}`;
-}
-const results = document.querySelector(".score__output");
-
-
-function playGame() {
-
-    // Play the game till someone has five points
-    playRound(getComputerChoice());
-
-    // console.log("Player Score: " + playerScore);
-    // console.log("Computer Score: " + computerScore)
-
-    // if (playerScore > computerScore) {
-    //     console.log("Humanity Wins!")
-    // }
-    // else if (computerScore < playerScore) {
-    //     console.log("Bow down to your AI overlords")
-    // }
-    // else {
-    //     console.log("It's a tie. Perhaps we need each other.")
-    // }
+    tiedScore.textContent = `Tied Score: ${tie}`;
+    roundTotal.textContent = `Total Rounds: ${round}`;
 }
